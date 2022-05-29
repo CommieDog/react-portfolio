ef53a79f0465de3eb77622cdae04d601985478cb
John Netzel <johnrnetzel@gmail.com>
2022-05-28 17:06:58 -0800

    Add content to header component

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,7 +1,25 @@
+import portrait from "../assets/images/profile-picture-small.jpg"
+
 function Header() {
     return (
-      <div>I am the header!
-      </div>
+      <header>
+        <h1>John Netzel</h1>
+        <img src={portrait} alt="Portrait of John Netzel" title="Hello there!"/>
+        <h2>Aspiring Web Developer</h2>
+        <div>
+          <p>3309 Rose Street</p>
+          <p>Anchorage, Alaska 99508</p>
+          <p>johnrnetzel@gmail.com</p>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#about-me">About Me</a></li>
+            <li><a href="#my-projects">My Projects</a></li>
+            <li><a href="#my-experience">My Experience</a></li>
+            <li><a href="#contact-me">Contact Me</a></li>
+          </ul>
+        </nav>
+      </header>
     );
   }
   
