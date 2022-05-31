03976ba507492e66d8cced334382ffc298261cc8
John Netzel <johnrnetzel@gmail.com>
2022-05-31 08:43:07 -0800

    Replace NavigationLink conponent with Navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,13 @@
-import NavbarLink from "./NavbarLink";
+import Navigation from "./Navigation";
 
 function Header(props) {
   return (
     <header>
       <h1>John Netzel</h1>
       <h2>Aspiring Web Developer</h2>
-      <nav>
-        <ul>
-          <li><NavbarLink name="About Me" isActivePage={props.isActivePage} onSetPage={props.onSetPage} /></li>
-          <li><NavbarLink name="Portfolio" isActivePage={props.isActivePage} onSetPage={props.onSetPage} /></li>
-          <li><NavbarLink name="Contact" isActivePage={props.isActivePage} onSetPage={props.onSetPage} /></li>
-          <li><NavbarLink name="Resume" isActivePage={props.isActivePage} onSetPage={props.onSetPage} /></li>
-        </ul>
-      </nav>
+      <Navigation isActivePage={props.isActivePage} onSetPage={props.onSetPage} />
     </header>
   );
 }
-  
+
 export default Header;
---
+++ src/components/Navigation.js
@@ -0,0 +1,36 @@
+function Navigation(props) {
+    /**
+     * Converts a name property into an appropriate href value
+     * @param {*} name the name of the NavbarLink
+     * @returns the href value of the NavbarLink
+     */
+    function getHref(name)
+    {
+      name = name.toLowerCase();
+      name = name.replace(" ", "-");
+      return "#" + name;
+    }
+  
+    // TODO replace with CSS class marker
+    function getPageMarker(pageName)
+    {
+      if(props.isActivePage(pageName))
+      {
+        return "!";
+      }
+      return "";
+    }
+
+    return (
+      <nav>
+        <ul>
+          <li><a href={getHref("About Me")} onClick={() => props.onSetPage("About Me")}>{"About Me" + getPageMarker("About Me")}</a></li>
+          <li><a href={getHref("Portfolio")} onClick={() => props.onSetPage("Portfolio")}>{"Portfolio" + getPageMarker("Portfolio")}</a></li>
+          <li><a href={getHref("Contact")} onClick={() => props.onSetPage("Contact")}>{"Contact" + getPageMarker("Contact")}</a></li>
+          <li><a href={getHref("Resume")} onClick={() => props.onSetPage("Resume")}>{"Resume" + getPageMarker("Resume")}</a></li>
+        </ul>
+      </nav>
+    );
+  }
+  
+  export default Navigation;
