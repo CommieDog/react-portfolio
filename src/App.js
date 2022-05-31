e4b92fd0568262cfb9c07f71a91740a0da8ae52f
John Netzel <johnrnetzel@gmail.com>
2022-05-30 16:07:05 -0800

    Fix naming convention for components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Header from "./containers/Header"
-import Body from "./containers/Body"
-import Footer from "./containers/Footer"
+import Header from "./components/Header"
+import Body from "./components/Body"
+import Footer from "./components/Footer"
 
 function App() {
   return (
