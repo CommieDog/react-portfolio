a51d9e62ec0eb80390cfd962a643886770ac5ef0
John Netzel <johnrnetzel@gmail.com>
2022-05-30 16:05:34 -0800

    Fix section titles

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -14,9 +14,9 @@ function Header() {
         <nav>
           <ul>
             <li><a href="#about-me">About Me</a></li>
-            <li><a href="#my-projects">My Projects</a></li>
-            <li><a href="#my-experience">My Experience</a></li>
-            <li><a href="#contact-me">Contact Me</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#resume">Resume</a></li>
           </ul>
         </nav>
       </header>
