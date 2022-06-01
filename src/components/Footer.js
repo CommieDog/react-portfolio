40cbf7ab87404fc2f126c73848ceb5912561fc48
John Netzel <johnrnetzel@gmail.com>
2022-06-01 06:39:01 -0800

    Add links to Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,11 @@
 function Footer() {
     return (
       <footer>
+        <ul>
+          <li><a href="https://github.com/CommieDog">GitHub</a></li>
+          <li><a href="https://www.linkedin.com/in/john-netzel-481112129/">LinkedIn</a></li>
+          <li><a href="https://stackoverflow.com/users/18535429/john-netzel">Stack Overflow</a></li>
+        </ul>
         <p>Copyright 2022 by John Netzel. All rights reserved.</p>
         <p>Licensed under the terms of the MIT License.</p>
       </footer>
